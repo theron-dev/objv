6aaf1a6df266301a63c14202ccf8e716454dc7e0
zhang hailong <hailongz.hit@gmail.com>
2014-02-08 21:47:26 +0800

    update objv

---
+++ include/objv_inifile.h
@@ -0,0 +1,14 @@
+//
+//  objv_inifile.h
+//  objv
+//
+//  Created by zhang hailong on 14-2-8.
+//  Copyright (c) 2014年 hailong.org. All rights reserved.
+//
+
+#ifndef objv_objv_inifile_h
+#define objv_objv_inifile_h
+
+
+
+#endif
---
+++ source/objv_inifile.c
@@ -0,0 +1,9 @@
+//
+//  objv_inifile.c
+//  objv
+//
+//  Created by zhang hailong on 14-2-8.
+//  Copyright (c) 2014年 hailong.org. All rights reserved.
+//
+
+#include <stdio.h>
