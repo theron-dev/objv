4215367eab8a3c853bb783b10b2c0a09c9ca0338
zhang hailong <hailongz.hit@gmail.com>
2014-03-10 19:06:06 +0800

    update

---
+++ include/objv_mail.h
@@ -0,0 +1,14 @@
+//
+//  objv_mail.h
+//  objv
+//
+//  Created by zhang hailong on 14-3-10.
+//  Copyright (c) 2014年 hailong.org. All rights reserved.
+//
+
+#ifndef objv_objv_mail_h
+#define objv_objv_mail_h
+
+
+
+#endif
---
+++ source/objv_mail.c
@@ -0,0 +1,9 @@
+//
+//  objv_mail.c
+//  objv
+//
+//  Created by zhang hailong on 14-3-10.
+//  Copyright (c) 2014年 hailong.org. All rights reserved.
+//
+
+#include <stdio.h>
