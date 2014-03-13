898fbbdd5d15046c2b15882f723cbbaf8f9d22a3
zhang hailong <hailongz.hit@gmail.com>
2014-03-13 12:27:17 +0800

    update objv

--- source/objv_crypt.c
+++ source/objv_crypt.c
@@ -254,6 +254,8 @@ objv_boolean_t objv_gzip_encode(void * data,size_t length,objv_mbuf_t * mbuf){
         size_t off = mbuf->length;
         z_stream stream ;
         
+        memset(& stream, 0, sizeof(z_stream));
+        
         stream.zalloc = Z_NULL;
         stream.zfree = Z_NULL;
         stream.opaque = Z_NULL;
@@ -261,9 +263,7 @@ objv_boolean_t objv_gzip_encode(void * data,size_t length,objv_mbuf_t * mbuf){
         stream.next_in = data;
         stream.total_out = 0;
         stream.avail_out = 0;
-        
-        memset(& stream, 0, sizeof(z_stream));
-        
+
         if (deflateInit2(&stream, Z_DEFAULT_COMPRESSION, Z_DEFLATED, 31, 8, Z_DEFAULT_STRATEGY) == Z_OK)
         {
             while (stream.avail_out == 0)
@@ -294,14 +294,14 @@ objv_boolean_t objv_gzip_decode(void * data,size_t length,objv_mbuf_t * mbuf){
         size_t off = mbuf->length;
         z_stream stream = {0};
         
+        memset(& stream, 0, sizeof(z_stream));
+        
         stream.zalloc = Z_NULL;
         stream.zfree = Z_NULL;
         stream.avail_in = (uInt)length;
         stream.next_in = (Bytef *)data;
         stream.total_out = 0;
         stream.avail_out = 0;
-        
-        memset(& stream, 0, sizeof(z_stream));
 
         if (inflateInit2(&stream, 47) == Z_OK)
         {
